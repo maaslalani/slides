5ac91bf1ddea678f56038547b56af5b20d174d9a
stoovon <murraystuart@mac.com>
2021-10-10 20:47:26 +0100

    Incorporate code review comments
    - Remove redundant comment.
    - Streamline "G" case.
    - Streamline "ClearVirtualText".
    - Include NumSlides within State for improved separation.

--- internal/navigation/navigation_test.go
+++ internal/navigation/navigation_test.go
@@ -12,6 +12,14 @@ func keyPresses(keys string) []string {
 	return strings.Split(keys, "")
 }
 
+func emptyBufferStateWithSlide(slide int) navigation.State {
+	return navigation.State{
+		Buffer:    "",
+		Slide:     slide,
+		NumSlides: 11,
+	}
+}
+
 func TestModel_UpdatePageNavigation(t *testing.T) {
 	tests := []struct {
 		name string
@@ -21,133 +29,80 @@ func TestModel_UpdatePageNavigation(t *testing.T) {
 		{
 			name: "Initial state",
 			keys: nil,
-			want: navigation.State{
-				Buffer: "",
-				Slide: 0,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(0),
 		},
 		{
 			name: "Can paginate",
 			keys: keyPresses("l"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 1,
-				ClearVirtualText: true,
-			},
+			want: emptyBufferStateWithSlide(1),
 		},
 		{
 			name: "Can paginate to end",
 			keys: keyPresses("jjjjjjjjjj"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 10,
-				ClearVirtualText: true,
-			},
+			want: emptyBufferStateWithSlide(10),
 		},
 		{
 			name: "Cannot paginate past end",
 			keys: keyPresses("jjjjjjjjjjjjj"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 10,
-				ClearVirtualText: true,
-			},
+			want: emptyBufferStateWithSlide(10),
 		},
 		{
 			name: "Can move to end",
 			keys: keyPresses("G"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 10,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(10),
 		},
 		{
 			name: "Can move to start",
 			keys: keyPresses("llgg"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 0,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(0),
 		},
 		{
 			name: "Repeats",
 			keys: keyPresses("2j"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 2,
-				ClearVirtualText: true,
-			},
+			want: emptyBufferStateWithSlide(2),
 		},
 		{
 			name: "Repeats with 0 (Vim ignores 0 and just does next command, so let's do the same)",
 			keys: keyPresses("0j"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 1,
-				ClearVirtualText: true,
-			},
+			want: emptyBufferStateWithSlide(1),
 		},
 		{
 			name: "Direct slide addressing sub min (Vim ignores minus, so let's do the same)",
 			keys: keyPresses("-11G"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 10,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(10),
 		},
 		{
 			name: "Direct slide addressing min",
 			keys: keyPresses("0G"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 0,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(0),
 		},
 		{
 			name: "Direct slide addressing in range",
 			keys: keyPresses("3G"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 2,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(2),
 		},
 		{
 			name: "Direct slide addressing max",
 			keys: keyPresses("11G"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 10,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(10),
 		},
 		{
 			name: "Direct slide addressing supra max",
 			keys: keyPresses("101G"),
-			want: navigation.State{
-				Buffer: "",
-				Slide: 10,
-				ClearVirtualText: false,
-			},
+			want: emptyBufferStateWithSlide(10),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			currentState := navigation.State{
-				Buffer:           "",
-				Slide:            0,
-				ClearVirtualText: false,
+				Buffer:    "",
+				Slide:     0,
+				NumSlides: 11,
 			}
-			numSlides := 11
 
 			for _, key := range tt.keys {
-				currentState = navigation.Navigate(currentState, key, numSlides)
+				currentState = navigation.Navigate(currentState, key)
 			}
 
 			assert.Equal(t, tt.want, currentState)
